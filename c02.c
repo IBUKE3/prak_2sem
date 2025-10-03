42126757b914d8ddf91805e878213c9ca11ce432
root <root@instance82157.waicore.network>
2025-10-03 21:42:10 +0200

    c02.c

---
+++ c02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+char* fgets( char* str, int count, FILE* stream );
+enum { MAX_LEN = 80 };
+
+
+int my_strlen(const char* str){
+  //char* p = (char *) str;
+  char* p = str;
+  while ((*p != '\0') && (*p != '\n')) p++;
+  return p-str;
+}
+
+
+int main(void){
+  char s[MAX_LEN+1]; // for '/0' at the end
+  if (fgets(s, MAX_LEN+1, stdin) == NULL) {
+    printf("EMPTY INPUT\n"); 
+    return 0;
+  }
+  printf("%d\n", my_strlen(s));
+  return 0;
+}
+
