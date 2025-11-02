9634d65653b33c2b7588c90704c592fc2c04f33c
root <root@instance82157.waicore.network>
2025-11-02 15:43:50 +0100

    f1.c

--- f/f1/f1.c
+++ f/f1/f1.c
@@ -4,21 +4,12 @@ int main (int argc, char* argv[]) {
   if (argc <= 1) return 1;
   FILE* f = fopen(argv[1], "r");
   
-  char c = fgetc(f);
+  int c;
+  char flag = 0;
   
-  while (c != EOF) {
-    while (c != EOF && c != '\n') {
-      c = fgetc(f);
-    }
-    if (c != EOF) c = fgetc(f);
-    
-    while (c != EOF && c != '\n') {
-      putchar(c);
-      c = fgetc(f);
-    }
-    if (c == '\n') putchar('\n'); 
-    if (c != EOF) c = fgetc(f);
-     
+  while ((c = fgetc(f)) != EOF) {
+    if (flag) putchar(c);
+    if (c == '\n') flag = !flag;
   }
 
   fclose(f);
