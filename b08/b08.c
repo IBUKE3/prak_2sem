dee318e31590c94ce0523940ac513e51ec0a5081
root <root@instance82157.waicore.network>
2025-11-02 14:35:41 +0100

    b08.c

--- b08/b08.c
+++ b08/b08.c
@@ -1,34 +1,32 @@
 
 int get_bit(unsigned char* m, size_t n) {
-  int t = n/8;
-  n%=8;
+  int t = n>>3;
+  n&=7;
   unsigned char c = m[t];
   return (c>>n)&1;
 }
 
 
 void set_bit(unsigned char* m, size_t n) {
-  int t = n/8;
-  n%=8;
+  int t = n>>3;
+  n&=7;
   unsigned char c = 1;
   c<<=n;
   m[t] |= c;
 }
 
 void clear_bit(unsigned char* m, size_t n) {
-  int t = n/8;
-  n%=8;
-  unsigned char c = 0xFF;
-  unsigned char c1 = 1;
-  c1 <<= n;
-  c ^= c1; // all ones, but zero in right position
-  m[t] &= c;
+  int t = n>>3;
+  n&=7;
+  unsigned char c = 1;
+  c1 <<= n; // all zeroes, bun one in necessary position
+  m[t] &= ~c1; // all ones, but zero in necessary position
 }
 
 
 void toggle_bit(unsigned char* m, size_t n) {
-  int t = n/8;
-  n%=8;
+  int t = n>>3;
+  n&=7;
   unsigned char c = 1;
   c<<=n;
   m[t] ^= c;
