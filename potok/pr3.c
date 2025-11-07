e26dd16194d120d69419625e0caec04c3815f2e5
root <golubevje@yandex.ru>
2025-11-07 21:47:29 +0100

    pr3.c

---
+++ potok/pr3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+typedef long long ll;
+
+int main(void) {
+  int a, b, n;
+  ll res = 0;
+
+  if (scanf("%d%d%d", &a, &b, &n) != 3) return 1;
+
+  for (int i = 0; i < n; i++) printf(" ");
+  for (int i = a; i < b; i++) {
+    printf(" %*d", n, i);
+  }
+  printf("\n");
+
+  for (int i = a; i < b; i++) {
+    printf("%*d", n, i);
+    for (int j = a; j < b; j++) {
+      res = i*j;
+      printf(" %*lld", n, res);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+
