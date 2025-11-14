521179d4844deca84f5b091cfbaeadd24b0e5215
IBUKE3 <golubevje@yandex.ru>
2025-11-14 21:34:18 +0100

    g6.c

---
+++ g/g6/g6.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void){
+  setbuf(stdin, 0);
+  char s, c;
+  s = fgetc(stdin);
+  while ((c = fgetc(stdin))!= -1) {
+    int r = fork(); 
+    if (r==0) {
+      if (c == s) {putc(c, stdout); putc(c, stdout);};
+      exit(0);
+    } else if (r==-1) {
+      wait(NULL);
+      if (fork()==0 && c==s) {putc(c, stdout); putc(c, stdout);};
+      exit(0);
+    }
+  }
+
+  
+  while (wait(NULL)!=-1) continue;
+  putc('\n', stdout);
+
+  return 0;
+}
+
