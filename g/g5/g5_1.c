da9ac1ec20a472834cb2eba75fce93ebad77ea3d
IBUKE3 <golubevje@yandex.ru>
2025-11-14 20:57:05 +0100

    g5_1.c

---
+++ g/g5/g5_1.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){
+  char** cq = argv;
+  int sz = 0;
+  while(*cq) {
+    sz++;
+  i cq++;
+  }
+  if (sz < 3) return 1;
+
+  cq = argv[sz-1];
+  
+  argv[sz-1] = NULL;
+
+  if (fork()==0) {
+    execv(argv[1], argv+1);
+    execvp(argv[1], argv+1);
+    exit(127);
+  }
+
+  int st = 0;
+  wait(&st);
+  if(WIFEXITED(st)&&WEXITSTATUS(st)==0) {
+    argv[sz-1] = cq;
+    if (fork()==0) {
+      execv(argv[sz-1], argv+sz-1);
+      execvp(argv[sz-1], argv+sz-1);
+      exit(127);
+    }
+  }
+
+
+  return 0;
+}
+
